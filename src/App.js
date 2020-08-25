453a3be03d00c5f9f8cbee05b4487519c450ff1c
maumainsaw <maumainsaw@gmail.com>
2020-08-24 21:59:56 -0500

    Creando Listado de Imagenes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, {useState,useEffect} from 'react';
 import Formulario from './components/Formulario'
+import ListadoImagenes from './components/ListadoImagenes'
 
 
 function App() {
 
   //State de la app
   const [busqueda,guardarBusqueda]=useState('');
+  const [imagenes, guardarImagenes]=useState([]);
 
   useEffect(() => {
     //no haga consulta si esta vacia
     if(busqueda === '') return;
 
    const consultarAPI = async () =>{
-    const imagenesPorPagina =30;
+    const imagenesPorPagina =15;
     const key ='18026964-37229ebf5f831553df0e61157';
     const url =`https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
     
     const respuesta = await fetch(url);
     const resultado = await respuesta.json();
-    console.log(resultado.hits)
+    guardarImagenes(resultado.hits)
 
    }
    consultarAPI()
@@ -30,10 +32,15 @@ function App() {
   return (
     <div className='container'>
       <div className='jumbotron'>
-        <h2 className='lead text-center '>Buscador de Imagenes</h2>
-        <Formulario
-        guardarBusqueda={guardarBusqueda}
-        />
+          <h2 className='lead text-center '>Buscador de Imagenes</h2>
+          <Formulario
+          guardarBusqueda={guardarBusqueda}
+          />
+      </div>
+      <div className='row justify-content-center'>
+          <ListadoImagenes
+            imagenes={imagenes}
+          />
       </div>
     </div>
   );
